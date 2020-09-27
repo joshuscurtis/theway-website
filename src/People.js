09234039379f53def9e14f0255dc673c5f6d24a0
Josh <joshuscurtis@gmail.com>
2020-09-27 23:37:07 +0100

    People.js

--- src/People.js
+++ src/People.js
@@ -17,27 +17,27 @@ export default function Pane() {
             text={baristaOne}
           />
         </div>
-        <div className="About__Card">
+        <div className="People__Card">
           <Card
-            img="https://www.christchurchdunstable.org.uk/wp-content/uploads/foodbank.jpg"
-            title="Foodbank"
-            text={bookclub}
+            img="https://goodcoffeeplace.com/wp-content/uploads/2019/06/Barista-Training.jpg?ezimgfmt=ngcb5/notWebP"
+            title="Barista One"
+            text={baristaOne}
+          />
+		  </div>
+        <div className="People__Card">
+          <Card
+            img="https://goodcoffeeplace.com/wp-content/uploads/2019/06/Barista-Training.jpg?ezimgfmt=ngcb5/notWebP"
+            title="Barista One"
+            text={baristaOne}
           />
         </div>
-        <div className="About__Card">
+        <div className="People__Card">
           <Card
-            img="https://www.christchurchdunstable.org.uk/wp-content/uploads/Game-on-snipnsketch.jpg"
-            title="Game On!"
-            text={gameon}
+            img="https://goodcoffeeplace.com/wp-content/uploads/2019/06/Barista-Training.jpg?ezimgfmt=ngcb5/notWebP"
+            title="Barista One"
+            text={baristaOne}
           />
-        </div>
-        <div className="About__Card">
-          <Card
-            img="https://www.christchurchdunstable.org.uk/wp-content/uploads/Cabin-snipnsketch.jpg"
-            title="Cabinfever!"
-            text={cabinfever}
-          />
-        </div>
+		  </div>
       </div>
     </div>
   );
