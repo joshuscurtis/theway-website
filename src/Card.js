35823484aec56a37e9242a0f627a40e2dd36f799
joshuscurtis <joshuscurtis@gmail.com>
2020-09-27 21:23:04 +0100

    sun night

--- src/Card.js
+++ src/Card.js
@@ -1,34 +1,40 @@
 import "./styles.css";
 
-import React from 'react';
-import { makeStyles } from '@material-ui/core/styles';
-import Card from '@material-ui/core/Card';
-import CardActionArea from '@material-ui/core/CardActionArea';
-import CardActions from '@material-ui/core/CardActions';
-import CardContent from '@material-ui/core/CardContent';
-import CardMedia from '@material-ui/core/CardMedia';
-import Button from '@material-ui/core/Button';
-import Typography from '@material-ui/core/Typography';
+import React from "react";
+import { makeStyles } from "@material-ui/core/styles";
+import Card from "@material-ui/core/Card";
+import CardActionArea from "@material-ui/core/CardActionArea";
+import CardActions from "@material-ui/core/CardActions";
+import CardContent from "@material-ui/core/CardContent";
+import CardMedia from "@material-ui/core/CardMedia";
+import Button from "@material-ui/core/Button";
+import Typography from "@material-ui/core/Typography";
 
 const useStyles = makeStyles({
   root: {
     maxWidth: 345,
+    height: 450
   },
   media: {
-    height: 140,
-  },
+    height: 140
+  }
 });
 
 export default function MediaCard(props) {
   const classes = useStyles();
-
+  var img =
+    "https://static.independent.co.uk/s3fs-public/thumbnails/image/2018/04/09/20/istock-157528129.jpg?width=1368";
+  if (props.img != null) {
+    img = props.img;
+  }
   return (
     <Card className={classes.root}>
       <CardActionArea>
         <CardMedia
           className={classes.media}
-          image="https://static.independent.co.uk/s3fs-public/thumbnails/image/2018/04/09/20/istock-157528129.jpg?width=1368"
-          title="Contemplative Reptile"
+          //image="https://static.independent.co.uk/s3fs-public/thumbnails/image/2018/04/09/20/istock-157528129.jpg?width=1368"
+          image={img}
+          title={props.title}
         />
         <CardContent>
           <Typography gutterBottom variant="h5" component="h2">
@@ -39,10 +45,7 @@ export default function MediaCard(props) {
           </Typography>
         </CardContent>
       </CardActionArea>
-      <CardActions>
-        <Button size="small" color="primary">
-          Share
-        </Button>
+      <CardActions className="Card__Actions">
         <Button size="small" color="primary">
           Learn More
         </Button>
--- src/Events.js
+++ src/Events.js
@@ -4,20 +4,26 @@ import Card from "./Card";
 export default function Pane() {
   const content =
     "The Way Coffee House is our newly restored 18th century coffee house, situated on West Street and the ideal place to meet friends, relax on your own or pick up a drink to take away.  Our friendly team serve fresh brewed coffee to suit all styles.  Our coffee is top quality, award winning, hand roasted and Fairtrade.  In addition to coffee we have quality teas, cold drinks, Zuma hot chocolate to satisfy any chocaholic, scrummy cakes and light refreshments – all at affordable prices – see below for our menu.";
+  const foodbank = "Dunstable Foodbank is an initiative hosted by a number of churches in our town. We are pleased as a church to serve our community in this venture providing food parcels to those in need. Christ Church Dunstable will be hosting a distribution centre at The Way on Tuesday and Friday afternoons between 2.30 & 4.30pm."
+  const bookclub = "Book Club takes place on the second Wednesday evening of every month at 7.45pm."
+  const cabinfever = "Cabin Fever is our Men’s Shed activity which is open to all. We regularly have demonstrations and talks about tools and techniques, and as equipment, materials and expertise increase, practical sessions will be held so you can try your hand at what you’ve learnt."
+  const gameon = "Game On! is for anyone who likes playing board games and meet with friends old and new while enjoying a selection of drinks and snacks from The Way Coffee House.  A selection of games are provided and you are welcome to bring your own."
+  
+  
   return (
     <div>
       <div className="Pane__Main">
         <div className="About__Card">
-          <Card title="About Us" text={content} />
+          <Card img="https://www.christchurchdunstable.org.uk/wp-content/uploads/Ministry-Posters-Book-Club.jpg" title="Book Club" text={bookclub} />
         </div>
         <div className="About__Card">
-          <Card title="About Us" text={content} />
+          <Card img="https://www.christchurchdunstable.org.uk/wp-content/uploads/foodbank.jpg" title="Foodbank" text={foodbank} />
         </div>
         <div className="About__Card">
-          <Card title="About Us" text={content} />
+          <Card img="https://www.christchurchdunstable.org.uk/wp-content/uploads/Game-on-snipnsketch.jpg" title="Game On!" text={gameon} />
         </div>
         <div className="About__Card">
-          <Card title="About Us" text={content} />
+          <Card img="https://www.christchurchdunstable.org.uk/wp-content/uploads/Cabin-snipnsketch.jpg" title="Cabinfever!" text={cabinfever} />
 		  </div>		
       </div>
     </div>
