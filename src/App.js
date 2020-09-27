e1b636d3a08f1d2e637902c4e77bde7a2bc8712e
joshuscurtis <joshuscurtis@gmail.com>
2020-09-27 22:06:22 +0100

    the

--- src/App.js
+++ src/App.js
@@ -3,18 +3,17 @@ import React from "react";
 import "./styles.css";
 import Main from "./Main";
 import About from "./About";
-import Events from "./Events"
-
+import Events from "./Events";
 
 export default function App() {
   return (
     <div className="App">
       <Main />
       <About />
-      <Events/>
-      <About/>
-      <About/>
-      <Events/>
+      <Events />
+      <About />
+      <About />
+      <Events />
     </div>
   );
 }
