cfb82e9072b2644983491b16c3f35d974663e3f4
joshuscurtis <joshuscurtis@gmail.com>
2020-09-28 19:11:17 +0100

    dinner

--- src/About.js
+++ src/About.js
@@ -1,20 +1,28 @@
 import React from "react";
 import "./About.css";
 
-import Box from "./Box";
+import { Box, OpeningTimes, News } from "./Box";
 
 export default function Pane() {
   const aboutUs =
     "The Way Coffee House is our newly restored 18th century coffee house, situated on West Street and the ideal place to meet friends, relax on your own or pick up a drink to take away.  Our friendly team serve fresh brewed coffee to suit all styles.  Our coffee is top quality, award winning, hand roasted and Fairtrade.  In addition to coffee we have quality teas, cold drinks, Zuma hot chocolate to satisfy any chocaholic, scrummy cakes and light refreshments – all at affordable prices – see below for our menu.";
-  const openingTimes = "08:30am to 3pm Mon-Sat";
-  const news = "Now servering Pancakes! "
-    return (
-      <div id="about" className="About__Main">
-        <Box title="About Us" content={aboutUs} />
-        <div className="About__row2">
-          <Box title="Opening Times" content={openingTimes} />
-          <Box title="Latest News" content={news} />  
+  const news = "Now servering Pancakes! ";
+  return (
+    <div id="about" className="About__Main">
+      <Box title="About Us" content={aboutUs} />
+      <div className="About__row2">
+        <OpeningTimes
+          title="Opening Times"
+          mon="08:30 - 15:00"
+          tue="08:30 - 15:00"
+          wed="08:30 - 15:00"
+          thu="08:30 - 15:00"
+          fri="08:30 - 15:00"
+          sat="08:30 - 15:00"
+          sun="CLOSED"
+        />
+        <News title="Latest News" content={news} />
       </div>
-      </div>
+    </div>
   );
 }
--- src/Box.js
+++ src/Box.js
@@ -8,7 +8,7 @@ import Typography from "@material-ui/core/Typography";
 
 import "./Box.css";
 
-export default function OutlinedCard(props) {
+export function Box(props) {
   return (
     <Card className="Box__Container" variant="outlined">
       <CardContent>
@@ -18,6 +18,63 @@ export default function OutlinedCard(props) {
         <Typography variant="body2" component="p">
           {props.content}
         </Typography>
+      </CardContent>
+      <CardActions></CardActions>
+    </Card>
+  );
+}
+
+export function News(props) {
+  return (
+    <Card className="Box__Container" variant="outlined">
+      <CardContent className="News">
+        <Typography className="Box__Title" variant="h5" component="h2">
+          {props.title}
+        </Typography>
+        <Typography variant="body2" component="p">
+          {props.content}
+        </Typography>
+      </CardContent>
+      <CardActions></CardActions>
+    </Card>
+  );
+}
+
+export function OpeningTimes(props) {
+  return (
+    <Card className="Box__Container" variant="outlined">
+      <CardContent className="OpeningTimes">
+        <Typography className="Box__Title" variant="h5" component="h2">
+          {props.title}
+        </Typography>
+        <p>
+          <b>Monday:</b> {props.mon}
+          <br></br>
+        </p>
+        <p>
+          <b>Tuesday</b>: {props.tue}
+          <br></br>
+        </p>
+        <p>
+          <b>Wednesday</b>: {props.wed}
+          <br></br>
+        </p>
+        <p>
+          <b>Thursday</b>: {props.thu}
+          <br></br>
+        </p>
+        <p>
+          <b>Friday</b>: {props.fri}
+          <br></br>
+        </p>
+        <p>
+          <b>Saturday</b>: {props.sat}
+          <br></br>
+        </p>
+        <p>
+          <b>Sunday</b>: {props.sun}
+          <br></br>
+        </p>
       </CardContent>
       <CardActions></CardActions>
     </Card>
