3d3b09dda2b2ec541b5fb3de2738fbed5260f30a
joshuscurtis <joshuscurtis@gmail.com>
2020-09-28 12:13:17 +0100

    jpoins

--- src/Main.js
+++ src/Main.js
@@ -1,14 +1,26 @@
 import React from "react";
 import Button from "@material-ui/core/Button";
 import "./styles.css";
-
+import Box from "./Box";
 export default function Panel() {
   return (
     <div className="Panel__Main">
       <div className="Panel__Box">
         <h1>The Way Coffee House</h1>
-        <Button className="Menu__Button" variant="contained" href="https://menu.theway.coffee">Our Menu</Button>
-        <Button variant="contained" href="https://ccd.churchsuite.co.uk/embed/calendar" >Our Events</Button>
+        <Button
+          className="Menu__Button"
+          variant="contained"
+          href="https://menu.theway.coffee"
+        >
+          Our Menu
+        </Button>
+        <Button
+          variant="contained"
+          href="https://ccd.churchsuite.co.uk/embed/calendar"
+        >
+          Our Events
+        </Button>
+        <Box className="News" title="Latest News" content="Exciting news!!" />
       </div>
     </div>
   );
--- src/PeopleCard.js
+++ src/PeopleCard.js
@@ -1,7 +1,6 @@
 import "./PeopleCard.css";
 
 import React from "react";
-import { makeStyles } from "@material-ui/core/styles";
 import Card from "@material-ui/core/Card";
 import CardActionArea from "@material-ui/core/CardActionArea";
 import CardActions from "@material-ui/core/CardActions";
@@ -10,27 +9,21 @@ import CardMedia from "@material-ui/core/CardMedia";
 import Button from "@material-ui/core/Button";
 import Typography from "@material-ui/core/Typography";
 
-const useStyles = makeStyles({
-  root: {
-    maxWidth: 345
-  },
-  media: {
-    height: 140
-  }
-});
-
 export default function MediaCard(props) {
-  const classes = useStyles();
   var img =
     "https://static.independent.co.uk/s3fs-public/thumbnails/image/2018/04/09/20/istock-157528129.jpg?width=1368";
   if (props.img != null) {
     img = props.img;
   }
   return (
-    <Card className="Card">
+    <Card className="PeopleCard">
       <CardActionArea>
-        <CardMedia className="PeopleCard__Image" image={img} title={props.title} />
-        <CardContent className="Card__Content">
+        <CardMedia
+          className="PeopleCard__Image"
+          image={img}
+          title={props.title}
+        />
+        <CardContent className="PeopleCard__Content">
           <Typography gutterBottom variant="h5" component="h2">
             {props.title}
           </Typography>
@@ -39,7 +32,7 @@ export default function MediaCard(props) {
           </Typography>
         </CardContent>
       </CardActionArea>
-      <CardActions className="Card__Actions">
+      <CardActions className="PeopleCard__Actions">
         <Button size="small" color="primary">
           Learn More
         </Button>
