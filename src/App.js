4dccf709970bcf33a59623435c6c33e23376fef2
joshuscurtis <joshuscurtis@gmail.com>
2020-09-29 17:46:33 +0100

    Final

--- src/App.js
+++ src/App.js
@@ -6,11 +6,13 @@ import About from "./About";
 import Events from "./Events";
 import People from "./People";
 
-
 import Footer from "./Footer";
+
+//set to top of page
 window.onbeforeunload = function () {
   window.scrollTo(0, 0);
 }
+
 export default function App() {
   return (
     <div className="App">
