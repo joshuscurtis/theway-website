a37964074ad2c61e369f99db5fa9471e3944de7c
joshuscurtis <joshuscurtis@gmail.com>
2020-09-30 12:28:51 +0100

    jump to top

--- src/App.js
+++ src/App.js
@@ -1,5 +1,8 @@
 import React from "react";
 
+import FloatingActionButton from "@material-ui/core/Fab";
+import AddIcon from "@material-ui/icons/ArrowUpward";
+
 import "./styles.css";
 import Header from "./Main";
 import About from "./About";
@@ -11,13 +14,29 @@ import Footer from "./Footer";
 //set to top of page
 window.onbeforeunload = function () {
   window.scrollTo(0, 0);
-}
+};
 
 export default function App() {
+  const style = {
+    margin: 0,
+    top: "auto",
+    right: 20,
+    bottom: 20,
+    left: "auto",
+    position: "fixed",
+    zIndex: 1000
+  };
+
+  function toTop() {
+    window.scrollTo(0, 0);
+  }
   return (
     <div className="App">
+      <FloatingActionButton onClick={toTop} style={style}>
+        <AddIcon />
+      </FloatingActionButton>
       <Header />
-      <About />      
+      <About />
       <Events />
       <People />
       <Footer />
