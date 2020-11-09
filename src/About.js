1f087032efb65d76454c4ec77ceea30dfb988e54
joshuscurtis <joshuscurtis@gmail.com>
2020-11-09 14:01:35 +0000

    text

--- src/About.js
+++ src/About.js
@@ -2,14 +2,16 @@ import React from "react";
 import "./About.css";
 import "animate.js";
 import { Box, OpeningTimes, News, Insta } from "./Box";
+import Typewriter from "typewriter-effect";
 
 import ScrollAnimation from "react-animate-on-scroll";
 import Carousel from "nuka-carousel";
 
 export default function Pane() {
   const aboutUs =
-    "The Way Coffee House is our newly restored 18th century coffee house, situated on West Street and the ideal place to meet friends, relax on your own or pick up a drink to take away.  Our friendly team serve fresh brewed coffee to suit all styles.  Our coffee is top quality, award winning, hand roasted and Fairtrade.  In addition to coffee we have quality teas, cold drinks, Zuma hot chocolate to satisfy any chocaholic, scrummy cakes and light refreshments – all at affordable prices – see below for our menu.";
+    "The Way Coffee House is our newly restored 18th century coffee house, situated on West Street and the ideal place to meet friends, relax on your own or pick up a drink to take away.  Our friendly team serve fresh brewed coffee to suit all styles. In addition to coffee we have quality teas, cold drinks, Zuma hot chocolate to satisfy any chocaholic, scrummy cakes and light refreshments";
   const news = "Now servering Pancakes! ";
+
   return (
     <div id="about">
       <div className="People__Title__Container">
@@ -69,6 +71,47 @@ export default function Pane() {
           >
             <News title="Latest Updates" content={news} />
           </ScrollAnimation>
+        </div>
+        <div className="About__row2">
+          <div className="exc">
+            <Typewriter
+              options={{ loop: true }}
+              onInit={(typewriter) => {
+                typewriter
+                  .typeString("Our coffee is ")
+                  .typeString(
+                    "<strong><span style='color: #00204e;'>top quality</span></strong>"
+                  )
+                  .pauseFor(300)
+                  .deleteChars(11)
+                  .typeString(
+                    "<strong><span style='color: #00204e;'>award winning</span></strong>"
+                  )
+                  .pauseFor(300)
+                  .deleteChars(13)
+                  .typeString(
+                    "<strong><span style='color: #00204e;'>hand roasted</span></strong>"
+                  )
+                  .pauseFor(300)
+                  .deleteChars(12)
+                  .typeString(
+                    "<strong><span style='color: #00204e;'>Fairtrade</span></strong>"
+                  )
+                  .pauseFor(300)
+                  .callFunction(() => {
+                    console.log(
+                      "<strong><span style='color: #00204e;'>String typed out!"
+                    );
+                  })
+                  .pauseFor(1000)
+                  .deleteAll()
+                  .callFunction(() => {
+                    console.log("All strings were deleted");
+                  })
+                  .start();
+              }}
+            />
+          </div>
         </div>
         <div className="About__row2">
           <ScrollAnimation animateOnce={true} animateIn="fadeIn">
