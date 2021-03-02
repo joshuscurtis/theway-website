043057cd839bf59af31bbb8ddef266041fe6c685
joshuscurtis <joshuscurtis@gmail.com>
2021-03-02 16:23:41 +0000

    insta feed hot fix

--- src/About.js
+++ src/About.js
@@ -111,11 +111,11 @@ export default function Pane() {
           </ScrollAnimation>
         </div>
 
-        <div className="About__row2">
-          <ScrollAnimation animateOnce={true} animateIn="fadeIn">
-            <Insta title="" />
-          </ScrollAnimation>
-        </div>
+     //   <div className="About__row2">
+     //     <ScrollAnimation animateOnce={true} animateIn="fadeIn">
+     //       <Insta title="" />
+     //     </ScrollAnimation>
+      //  </div>
       </div>
     </div>
   );
